882d3189e0c43d73a6adcea9697a4bea0d47500e
Nima <bg.nima@gmail.com>
2017-03-06 12:37:49 +0330

    added swipe for touch devices

--- dist/js/VestaDatePicker.js
+++ dist/js/VestaDatePicker.js
@@ -17,7 +17,7 @@
             that = this;
         mainContainer.append(container).addClass('ui-vestadp-maincontainer')
                      .addClass(settings.showInline ? 'ui-vestadp-inline' : 'ui-vestadp-popup');
-        bindClicks();
+        bindEventHandlers();
         mouseWheelBinder(container);
 
         var clickHandlers = [
@@ -305,7 +305,7 @@
             calTable.fadeIn();
         }
 
-        function bindClicks() {
+        function bindEventHandlers() {
             $(container).on('click','[data-event="click"]',function () {
                 if ($(this).attr('disabled')) return;
                 var handler = $(this).attr("data-handler");
@@ -314,7 +314,16 @@
                 if (runAfter) {
                     clickHandlers[currentView]["after"].call(this,args);
                 }
-            });            
+            });   
+
+            swipedetect(container[0], function (direction) {
+                if (direction=='none') return true;
+                
+                var runAfter = clickHandlers[currentView][direction=='right' ? 'next' : 'prev'].call(this);
+                if (runAfter) {
+                    clickHandlers[currentView]['after'].call(this);
+                }
+            });   
         }
 
         function renderHeader(title, args, opts) {
@@ -631,7 +640,52 @@
         }
     };
 
-
+    function swipedetect(el, callback){
+    
+        var touchsurface = el,
+        swipedir,
+        startX,
+        startY,
+        distX,
+        distY,
+        threshold = 150, //required min distance traveled to be considered swipe
+        restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+        allowedTime = 300, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime,
+        handleswipe = callback || function(swipedir){}
+    
+        touchsurface.addEventListener('touchstart', function(e){
+            var touchobj = e.changedTouches[0]
+            swipedir = 'none'
+            dist = 0
+            startX = touchobj.pageX
+            startY = touchobj.pageY
+            startTime = new Date().getTime() // record time when finger first makes contact with surface
+            //e.preventDefault()
+        }, false)
+    
+        touchsurface.addEventListener('touchmove', function(e){
+            e.preventDefault() // prevent scrolling when inside DIV
+        }, false)
+    
+        touchsurface.addEventListener('touchend', function(e){
+            var touchobj = e.changedTouches[0]
+            distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+            distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+            elapsedTime = new Date().getTime() - startTime // get time elapsed
+            if (elapsedTime <= allowedTime){ // first condition for awipe met
+                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                    swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+                }
+                else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                    swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+                }
+            }
+            handleswipe(swipedir)
+            //e.preventDefault()
+        }, false)
+    }
 
     vestaDatePicker.defaultSettings = {
         direction: "rtl",
